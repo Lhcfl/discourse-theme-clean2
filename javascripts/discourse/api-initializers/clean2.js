2a9f73a19a40dc60834b86f7884b664bc468a47a
Lhcfl <Lhcfl@outlook.com>
2023-12-07 23:23:37 +0800

    fix: add clickable topics

--- javascripts/discourse/api-initializers/clean2.js
+++ javascripts/discourse/api-initializers/clean2.js
@@ -1,40 +1,71 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
+const pluginId = "clean2";
+
+function makeTopicClickable(api) {
+  api.modifyClass(
+    "component:topic-list-item",
+    {
+      unhandledRowClick(e, topic) {
+        if (e.target.tagName.toLowerCase() === "img") {
+          return;
+        }
+        if (e.target.tagName.toLowerCase() === "a") {
+          return;
+        }
+        this.navigateToTopic(
+          topic,
+          topic.linked_post_number
+            ? topic.urlForPostNumber(topic.linked_post_number)
+            : topic.get("lastUnreadUrl")
+        );
+      },
+    },
+    {
+      pluginId,
+    }
+  );
+}
+
+function addClassWhenScroll(api) {
+  const site = api.container.lookup("site:main");
+  if (!site.mobileView) {
+    return;
+  }
+  let scrollTop = window.scrollY;
+  const body = document.body;
+  const scrollMax = 0;
+  let lastScrollTop = 0;
+  const hiddenNavClass = "nav-controls-hidden";
+
+  const add_class_on_scroll = () => body.classList.add(hiddenNavClass);
+  const remove_class_on_scroll = () => body.classList.remove(hiddenNavClass);
+
+  window.addEventListener("scroll", function () {
+    scrollTop = window.scrollY;
+    if (
+      lastScrollTop < scrollTop &&
+      scrollTop > scrollMax &&
+      !body.classList.contains(hiddenNavClass)
+    ) {
+      add_class_on_scroll();
+    } else if (
+      lastScrollTop > scrollTop &&
+      body.classList.contains(hiddenNavClass)
+    ) {
+      remove_class_on_scroll();
+    }
+    lastScrollTop = scrollTop;
+  });
+}
+
 export default {
   name: "discourse-navigation-controls",
 
   initialize() {
     withPluginApi("0.8.13", (api) => {
-      const site = api.container.lookup("site:main");
-      if (!site.mobileView) {
-        return;
-      }
-      let scrollTop = window.scrollY;
-      const body = document.body;
-      const scrollMax = 0;
-      let lastScrollTop = 0;
-      const hiddenNavClass = "nav-controls-hidden";
-
-      const add_class_on_scroll = () => body.classList.add(hiddenNavClass);
-      const remove_class_on_scroll = () =>
-        body.classList.remove(hiddenNavClass);
-
-      window.addEventListener("scroll", function () {
-        scrollTop = window.scrollY;
-        if (
-          lastScrollTop < scrollTop &&
-          scrollTop > scrollMax &&
-          !body.classList.contains(hiddenNavClass)
-        ) {
-          add_class_on_scroll();
-        } else if (
-          lastScrollTop > scrollTop &&
-          body.classList.contains(hiddenNavClass)
-        ) {
-          remove_class_on_scroll();
-        }
-        lastScrollTop = scrollTop;
-      });
+      addClassWhenScroll(api);
+      makeTopicClickable(api);
     });
   },
 };
