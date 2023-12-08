0e86aa2b2a109ae9af1d90aaf5ec76014bba6364
Lhcfl <Lhcfl@outlook.com>
2023-12-08 13:30:34 +0800

    feat: add rightbar site card

--- javascripts/discourse/templates/connectors/after-main-outlet/rightbar.js
+++ javascripts/discourse/templates/connectors/after-main-outlet/rightbar.js
@@ -13,20 +13,28 @@ export default {
   setupComponent(attrs, component) {
     if (!this.site.mobileView) {
       withPluginApi("0.8.7", (api) => {
-        // script wont run unless user is logged in
-        if (api.getCurrentUser() === null) {
-          return false;
+        if (!api.getCurrentUser()) {
+          component.set("user", {
+            isSiteUser: true,
+            summary_url: "/about",
+            profile_background_upload_url:
+              settings.rightbar_site_background_url || undefined,
+            name: this.siteSettings.title,
+            username: this.siteSettings.title,
+            avatar_template: this.siteSettings.logo_small,
+            bio_cooked: settings.rightbar_site_desctiption,
+          });
+        } else {
+          component.set("user", api.getCurrentUser());
+
+          let username = component.get("currentUser.username");
+
+          ajax("/u/" + username + ".json").then((res) => {
+            addRequiredUserInfo(res.user);
+
+            component.set("user", res.user);
+          });
         }
-
-        component.set("user", api.getCurrentUser());
-
-        let username = component.get("currentUser.username");
-
-        ajax("/u/" + username + ".json").then((res) => {
-          addRequiredUserInfo(res.user);
-
-          component.set("user", res.user);
-        });
       });
     }
   },
